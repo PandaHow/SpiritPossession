f696d1a69c1d4dc60fe74ed6a9ab7ccf35a6180f
从来不写bug程序永远不会出错努力做毕设的PandaHow <panda_how@foxmail.com>
2017-07-02 01:09:24 +0800

    先学一下unet做demo photonserver的好友系统待完善

---
+++ SpritePossession/Assets/Scenes/GameTest/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public GameObject x;
+    public float speed = 1f;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.A))
+            x.GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0);
+        if (Input.GetKeyDown(KeyCode.W))
+            x.GetComponent<Rigidbody2D>().velocity = new Vector2(-speed, 0);
+    }
+}
---
+++ SpritePossession/Assets/Scenes/GameTest/光标控制.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class 光标控制 : MonoBehaviour {
+    float h1, h2, v1, v2;
+	void Start () {
+        Cursor.visible = false;
+	}
+
+    public KeyCode getKeyDownCode()
+    {
+        if (Input.anyKeyDown)
+        {
+            foreach (KeyCode keyCode in Enum.GetValues(typeof(KeyCode)))
+            {
+                if (Input.GetKeyDown(keyCode))
+                {
+                    Debug.Log(keyCode.ToString());
+                    return keyCode;
+                }
+            }
+        }
+        return KeyCode.None;
+    }
+
+    void Update()
+    {
+        getKeyDownCode();
+        float hl = Input.GetAxis("Horizontal_Left");
+        float vl = Input.GetAxis("Vertical_Left");
+        float x = Input.GetAxis("Xbox +X");
+        float y = Input.GetAxis("Xbox +Y");
+        float hr = Input.GetAxis("Horizontal_Right");
+        float vr = Input.GetAxis("Vertical_Right");
+        float t = Input.GetAxis("LRT");
+        if (Mathf.Abs(hl) > 0.05f || Mathf.Abs(vl) > 0.05f)
+        {
+            print("左摇杆leftX:" + hl);
+            print("左摇杆leftY:" + vl);
+        }
+
+        if (Mathf.Abs(x) > 0.05f || Mathf.Abs(y) > 0.05f)
+        {
+            print("十字键Xbox +X:" + x);
+            print("十字键Xbox +Y:" + y);
+        }
+
+        if (Mathf.Abs(hr) > 0.05f || Mathf.Abs(vr) > 0.05f)
+        {
+            print("右摇杆RightX:" + hr);
+            print("右摇杆RightY:" + vr);
+        }
+
+        if (Mathf.Abs(t) > 0.05f)
+        {
+            print("LRT:" + t);
+        }
+
+    }
+}
